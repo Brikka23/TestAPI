cfd25d81f8898c49a12c44c93748958ebc01da9e
Brikka23 <ilyabogdanov787@gmail.com>
2022-10-02 23:58:30 +0300

    bug fix

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -61,16 +61,6 @@ class Todo
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
-public class TodoItemDTO
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-
-    public TodoItemDTO() { }
-    public TodoItemDTO(Todo todoItem) =>
-    (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
-}
 
 class TodoDb : DbContext
 {
